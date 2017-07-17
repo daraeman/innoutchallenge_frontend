62f06668d7b95a8e8da8e2abcf98a691a53f9da0
daraeman <daraeman@gmail.com>
2017-07-17 12:34:48 -0700

    hide sidebar when clicking links

--- src/js/components/TopNav.js
+++ src/js/components/TopNav.js
@@ -43,10 +43,10 @@ export default class TopNav extends React.Component {
 				</nav>
 				<div id="side_nav_clickfield" class={ this.state.sidebarClass } onClick={ this.sidebarToggle }></div>
 				<ul id="side_nav" class={ this.state.sidebarClass }>
-					<li><NavLink to="/challengers" activeClassName="active">Challengers</NavLink></li>
-					<li><NavLink to="/map" activeClassName="active">Map</NavLink></li>
-					<li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-					<li><NavLink to="/account" activeClassName="active">Account</NavLink></li>
+					<li><NavLink to="/challengers" activeClassName="active" onClick={ this.sidebarToggle }>Challengers</NavLink></li>
+					<li><NavLink to="/map" activeClassName="active" onClick={ this.sidebarToggle }>Map</NavLink></li>
+					<li><NavLink to="/about" activeClassName="active" onClick={ this.sidebarToggle }>About</NavLink></li>
+					<li><NavLink to="/account" activeClassName="active" onClick={ this.sidebarToggle }>Account</NavLink></li>
 				</ul>
 			</div>
 		)
